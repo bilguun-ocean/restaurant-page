c11dd364a406e2259645e4c64ce727fac1c298af
Bilguun13 <83194126+Bilguun13@users.noreply.github.com>
2022-08-11 18:50:25 +0800

    skeleton for webpage

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-alert("JavaScript Loaded!")
+import './style.css';
+
+/* SKELETON */
+
+const mainContainer = document.querySelector('.content');
+
+// Tabs section
+const tabsContainer = document.createElement('div');
+tabsContainer.classList.add('tab-container')
+
+function createTabElement(name) {
+  let element = document.createElement('div');
+  element.textContent = name;
+  element.classList.add("tab")
+  return element
+}
+
+tabsContainer.appendChild(createTabElement("Home"))
+tabsContainer.appendChild(createTabElement("Menu"))
+tabsContainer.appendChild(createTabElement("Location"))
+
+mainContainer.appendChild(tabsContainer)
